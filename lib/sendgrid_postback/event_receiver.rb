01064f5896c61b09a74ce30e74d882d90186fd24
Doug Puchalski <doug316@gmail.com>
2012-06-18 09:55:08 -0700

    Typo

--- lib/sendgrid_postback/event_receiver.rb
+++ lib/sendgrid_postback/event_receiver.rb
@@ -7,19 +7,19 @@ module SendgridPostback
     # Serialize if using ActiveRecord
     attr_accessor :sendgrid_events
     attr_accessor :sendgrid_state
-      
+
     def post_sendgrid_event event_data
-      SendgridPostaback.logger.info "Posted event data #{event_data.inspect}"
+      SendgridPostback.logger.info "Posted event data #{event_data.inspect}"
       sendgrid_events ||= []
       sendgrid_events << event_data
       sendgrid_state = Event.sorted(sendgrid_events).last['event']
       after_create_sendgrid_event(event_data)
     end
-    
+
     # Override hook as necessary
     def after_create_sendgrid_event(event_data)
     end
 
   end
-  
+
 end
